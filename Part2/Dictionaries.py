380d6ea6622efbadf4674b6f172c8fd4a771091d
VEDgit2005 <jenavedant@gmail.com>
2025-03-04 22:15:08 +0530

    Dictionary file updated

--- Part2/Dictionaries.py
+++ Part2/Dictionaries.py
@@ -1,5 +1,20 @@
 #name = {key:value} = dictionary
-people={'ppatel':'parth patel',
-        'zmin':'zhing min'}
+people = {'ppatel': 'parth patel',
+          'zmin': 'zhing min',
+          'htanaka': 'Haru Tanaka'}
+for person in people:
+    print(person + " is the key of the value " +  people[person])
+
 print(people)
 print(people['ppatel'])
+
+# Popping (deleting) the element
+people.pop('zmin')
+print(people)
+print(len(people))
+
+# Adding the popped element back using a dictionary
+people.update({'zmin': 'zhing min'})
+print(people)
+print(len(people))
+
